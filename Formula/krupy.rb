4840371eb21ae0de4a4b013187f8ecfd4bbef1cd
Krunal Kevadiya <krunal.kevadiya@gmail.com>
2024-02-21 13:23:30 +0530

    update bottle from formula file

--- Formula/krupy.rb
+++ Formula/krupy.rb
@@ -8,14 +8,14 @@ class Krupy < Formula
   head "https://github.com/Krunal-Kevadiya/krupy.git", branch: "main"
   bottle do
     root_url "https://github.com/Krunal-Kevadiya/krupy/releases/download/2.0.0"
-    # sha256 cellar: :any, arm64_sonoma: "b9731386cf74f86d994f610269d2732417ec4e7dcd4ed982c37c8ee6e84f77df"
+    sha256 cellar: :any, arm64_sonoma: "36591153b5fe3c82cea19b87f27e019fe61414aa08601913cdd26115d20804f4"
     # sha256 cellar: :any, arm64_ventura: "ce595634fbdb0d203793caac6a43b62d599aa876e00b936a4fc24d35ab79b15c"
     # sha256 cellar: :any, arm64_monterey: "4e39d908acf964c2b1a08964f3c44e4adf79c602bf68c8c1e4766cce374941f1"
-    # sha256 cellar: :any, sonoma: "8319c3a9cbedccdafbff303dd76baf8df155594fbb42cbf757542dec04b55afd"
-    # sha256 cellar: :any, ventura: "5ead52960bd8b00540d21dff16cf3d2c50a6e5a0eea1c2e12660e5d44d62264a"
-    # sha256 cellar: :any, monterey: "41102955868146867a074fc95d908b99e1faca5165a648fc81ae812c240a9a65"
-    # sha256 cellar: :any_skip_relocation,
-    # x86_64_linux: "9e344dc0250c8f6f524bdc0a2cca20d84d8a998912e6af92dc734af3f38f5ae7"
+    # sha256 cellar: :any, sonoma: "36591153b5fe3c82cea19b87f27e019fe61414aa08601913cdd26115d20804f4"
+    sha256 cellar: :any, ventura: "721b75d01290a8cfeab4f3db6f32075b2a3e3e1c952af7f321ea9eca4114e463"
+    sha256 cellar: :any, monterey: "05eaac719fc9593ce88dc53751b85b52a1675835d2ae8ea6bb409b4503447b70"
+    sha256 cellar: :any_skip_relocation,
+    x86_64_linux: "4e333548397782c1a743018516a781d9289f2dcfefba7b528628927f2e276d62"
   end
   depends_on "rust" => :build
   depends_on "pygments"
